039489766e3d64e374ab5c4a70581838ac9b23cd
Olena Piankova <el.piankova@gmail.com>
2018-12-02 23:14:52 +0200

    Added the same test using low-level code for comparison (only hardcore :))

--- conftest.py
+++ conftest.py
@@ -16,3 +16,41 @@ def login_user(app):
     app.logout('admin')
 
 
+# Фикстуры для теста написанного низкоуровневым кодом
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from page_objects import locators
+from selenium.webdriver import ActionChains
+
+
+@pytest.fixture()
+def driver():
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(2)
+    driver.maximize_window()
+    driver.get('http://127.0.0.1/oxwall/')
+    yield driver
+    driver.quit()
+
+
+@pytest.fixture()
+def login_low_level(driver):
+    username = "admin"
+    password = "pass"
+    driver.find_element(*locators.SIGN_IN_MENU).click()
+    login = driver.find_element(*locators.LOGIN_FIELD)
+    login.click()
+    login.send_keys(username)
+    passw = driver.find_element(*locators.PASS_FIELD)
+    passw.click()
+    passw.send_keys(password)
+    driver.find_element(*locators.SIGN_IN_BUTTON).click()
+    WebDriverWait(driver, 10).until(EC.invisibility_of_element_located(locators.LOGIN_BACKGROUND))
+    yield
+    # Logout
+    menu = driver.find_element(By.LINK_TEXT, username.title())
+    ActionChains(driver).move_to_element(menu).perform()
+    driver.find_element(By.XPATH, './/a[contains(@href,"sign-out")]').click()
+
--- test_comment_create.py
+++ test_comment_create.py
@@ -5,3 +5,23 @@ def test_add_comment(app, login_user):
     # Verifications
     comments_elements = app.get_comments()
     assert comments_elements[0].text == expected_text
+
+
+# Тот же тест написанній низкоуровневым кодом (для сравнения)
+import time
+
+def test_add_comment2(driver, login_low_level):
+    test_text = "Shit happens!!!:("
+    expected_text = test_text[-2]
+    newsfeed = driver.find_element_by_name("status")
+    newsfeed.click()
+    newsfeed.clear()
+    newsfeed.click()
+    newsfeed.send_keys(test_text)
+    send_button = driver.find_element_by_name("save")
+    send_button.click()
+    time.sleep(2)
+    # Verifications
+    text_element = driver.find_elements_by_class_name("ow_newsfeed_content")
+    print(text_element[0].text)
+    assert text_element[0].text == expected_text
