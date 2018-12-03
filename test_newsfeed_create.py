7469f6616d3c73f7e276cc4a773ad7f2500932fd
Olena Piankova <el.piankova@gmail.com>
2018-12-04 01:42:26 +0200

    Added comments for explanation code action

--- test_newsfeed_create.py
+++ test_newsfeed_create.py
@@ -3,9 +3,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 
+# Open browser and Oxwall site, driver settings
 driver = webdriver.Chrome()
 driver.get('http://127.0.0.1/oxwall/')
-time.sleep(5)
+driver.implicitly_wait(5)
+driver.maximize_window()
+
+# Login
 driver.find_element(By.XPATH, '//*[contains(@id,"console_item")]/span[1]').click()
 login = driver.find_element(By.NAME, 'identity')
 login.click()
@@ -15,32 +19,32 @@ passw.click()
 passw.send_keys('pass')
 driver.find_element(By.XPATH, "//div[@class='ow_right']").click()
 
+# Wait until login finishing
 time.sleep(5)
 
+# Write some text to Newsfeed form and send it
 newsfeed = driver.find_element_by_name("status")
 newsfeed.click()
 newsfeed.clear()
 newsfeed.click()
 
 test_text = "Shit happens!!!:("
-expected_text = test_text[-2]
-
 newsfeed.send_keys(test_text)
 send_button = driver.find_element_by_name("save")
 send_button.click()
 
 time.sleep(2)
 
+# Verification that new status with this text appeared
 text_element = driver.find_elements_by_class_name("ow_newsfeed_content")
-
-print(text_element[0].text)
-
-
+expected_text = test_text[-2]
 assert text_element[0].text == expected_text
 
+#Logout
 menu = driver.find_element(By.LINK_TEXT, 'Admin')
 actions = ActionChains(driver)
 actions.move_to_element(menu).perform()
 driver.find_element(By.XPATH, './/a[contains(@href,"sign-out")]').click()
 
+# Close browser
 driver.close()
