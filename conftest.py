8318c233289682adcd123d0d4663a046a9467ef5
Olena Piankova <el.piankova@gmail.com>
2018-12-04 21:23:39 +0200

    How test and fixture look like now when we use high-level method!

--- conftest.py
+++ conftest.py
@@ -1,23 +1,22 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from locators import locator
+from oxwall_site_model import OxwallSite
 
 
 @pytest.fixture()
 def driver():
-    # Open browser and Oxwall site, driver settings
+    # Open browser driver settings
     driver = webdriver.Chrome()
-    driver.get('http://127.0.0.1/oxwall/')
     driver.implicitly_wait(5)
     driver.maximize_window()
     yield driver
     # Close browser
     driver.quit()
 
+@pytest.fixture()
+def app(driver):
+    app = OxwallSite(driver)
+    return app
 
 @pytest.fixture()
 def user():
@@ -25,26 +24,7 @@ def user():
 
 
 @pytest.fixture()
-def login(driver, user):
-    # Login
-    driver.find_element(*locator.SIGN_IN_MENU).click()
-    login = driver.find_element(*locator.LOGIN_FIELD)
-    login.click()
-    login.send_keys(user["username"])
-    passw = driver.find_element(*locator.PASS_FIELD)
-    passw.click()
-    passw.send_keys(user["password"])
-    driver.find_element(*locator.SIGN_IN_BUTTON).click()
-    # Wait until grey background disappeared
-    wait = WebDriverWait(driver, 5)
-    wait.until(EC.invisibility_of_element_located(locator.LOGIN_BACKGROUND))
-
-
-@pytest.fixture()
-def logout(driver, user):
-    yield             # Because we need to do it after test
-    # Logout
-    menu = driver.find_element(By.LINK_TEXT, user["username"].title())
-    actions = ActionChains(driver)
-    actions.move_to_element(menu).perform()
-    driver.find_element(By.XPATH, './/a[contains(@href,"sign-out")]').click()
+def sign_in_session(app, user):
+    app.login_as(user)
+    yield
+    app.logout_as(user)
--- test_newsfeed_create.py
+++ test_newsfeed_create.py
@@ -1,22 +1,9 @@
-import time
-
-
-def test_add_text_status(driver, login, logout):
+def test_add_text_status(app, sign_in_session):
     test_text = "Shit happens!!!:("
     expected_text = test_text[-2]
 
-    # Write some text to Newsfeed form and send it
-    newsfeed = driver.find_element_by_name("status")
-    newsfeed.click()
-    newsfeed.clear()
-    newsfeed.click()
-    newsfeed.send_keys(test_text)
-    send_button = driver.find_element_by_name("save")
-    send_button.click()
-
-    # Wait until new status appear
-    time.sleep(2)
-
+    app.add_new_text_status(test_text)
+    app.wait_until_new_status_appeared()
     # Verification that new status with this text appeared
-    text_element = driver.find_elements_by_class_name("ow_newsfeed_content")
-    assert text_element[0].text == expected_text
+    text_elements = app.get_newsfeed_list()
+    assert text_elements[0].text == expected_text
