2a2f57e70565776fa0d261305afdfbe4ef9bc42f
Olena Piankova <el.piankova@gmail.com>
2018-12-12 19:19:50 +0200

    Один из способов систематизации локаторов

--- locators/locator.py
+++ locators/locator.py
@@ -1,14 +1,22 @@
 from selenium.webdriver.common.by import By
 
-# Main menu locators
-SIGN_IN_MENU = (By.XPATH, '//*[contains(@id,"console_item")]/span[1]')
-SIGN_OUT_MENU = ()
 
-# Login windows locators
-LOGIN_FIELD = (By.NAME, 'identity')
-PASS_FIELD = (By.NAME, 'password')
-SIGN_IN_BUTTON = (By.XPATH, "//div[@class='ow_right']")
-LOGIN_BACKGROUND = (By.ID, "floatbox_overlay")
+class InternalPageLocators:
+    ACTIVE_MENU = (By.XPATH, "//div[contains(@class, 'ow_menu_wrap')]//li[contains(@class, 'active')]")
+    DASHBOARD_MENU = (By.LINK_TEXT, "DASHBOARD")
+    MAIN_MENU = (By.LINK_TEXT, "MAIN")
+    PHOTO_MENU = ()  # TODO
+    # TODO Add other menu locators
+    SIGN_IN_MENU = (By.XPATH, '//*[contains(@id,"console_item")]/span[1]')
+    USER_MENU = (By.XPATH, "//div[contains(@class,'ow_console_dropdown_hover')]")
+    SIGN_OUT_LINK = (By.XPATH, './/a[contains(@href,"sign-out")]')
+
+
+class SignInLocators:
+    LOGIN_FIELD = (By.NAME, 'identity')
+    PASS_FIELD = (By.NAME, 'password')
+    SIGN_IN_BUTTON = (By.XPATH, "//div[@class='ow_right']")
+    LOGIN_BACKGROUND = (By.ID, "floatbox_overlay")
 
 # Newsfeed locators ...
 
--- oxwall_site_model.py
+++ oxwall_site_model.py
@@ -2,7 +2,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
-from locators import locator
+from locators.locator import InternalPageLocators, SignInLocators
 import time
 
 
@@ -17,22 +17,22 @@ class OxwallSite:
     def login_as(self, user):
         """ Login to Oxwall site by user"""
         driver = self.driver
-        driver.find_element(*locator.SIGN_IN_MENU).click()
-        login = driver.find_element(*locator.LOGIN_FIELD)
+        driver.find_element(*InternalPageLocators.SIGN_IN_MENU).click()
+        login = driver.find_element(*SignInLocators.LOGIN_FIELD)
         login.click()
         login.send_keys(user.username)
-        passw = driver.find_element(*locator.PASS_FIELD)
+        passw = driver.find_element(*SignInLocators.PASS_FIELD)
         passw.click()
         passw.send_keys(user.password)
-        driver.find_element(*locator.SIGN_IN_BUTTON).click()
+        driver.find_element(*SignInLocators.SIGN_IN_BUTTON).click()
         # Wait until grey background disappeared
         wait = WebDriverWait(driver, 5)
-        wait.until(EC.invisibility_of_element_located(locator.LOGIN_BACKGROUND))
+        wait.until(EC.invisibility_of_element_located(SignInLocators.LOGIN_BACKGROUND))
 
     def logout_as(self, user):
-        menu = self.driver.find_element(By.LINK_TEXT, user.username.title())
+        menu = self.driver.find_element(*InternalPageLocators.USER_MENU)
         self.actions.move_to_element(menu).perform()
-        self.driver.find_element(By.XPATH, './/a[contains(@href,"sign-out")]').click()
+        self.driver.find_element(*InternalPageLocators.SIGN_OUT_LINK).click()
 
     def add_new_text_status(self, text):
         driver = self.driver
