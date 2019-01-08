cd8ac0052c5c7647b1daf60c37d5c09dc56a58a4
Olena Piankova <el.piankova@gmail.com>
2019-01-08 16:57:55 +0200

    Changed 2nd test for Status creation using Page Objects pattern

--- page_objects/dashboard_page.py
+++ page_objects/dashboard_page.py
@@ -1,10 +1,31 @@
+import time
+from selenium.webdriver.common.by import By
+
 from page_objects.internal_page import InternalPage
 
 
 class DashboardPage(InternalPage):
-    pass
+    STATUS_TEXT_FIELD = (By.NAME, "status")
+    SEND_BUTTON = (By.NAME, "save")
+    STATUS_BOX = (By.XPATH, "//li[contains(@id, 'action-feed')]")
 
-    # TODO Add all elements and actions that you have in Main Page
+    # TODO Add all elements and actions that you have in Dashboard Page
 
     def is_this_page(self):
         return self.active_menu.text == "DASHBOARD"
+
+    @property
+    def status_text_field(self):
+        return self.find_visible_element(self.STATUS_TEXT_FIELD)
+
+    @property
+    def send_button(self):
+        return self.find_visible_element(self.SEND_BUTTON)
+
+    @property
+    def status_list(self):
+        return self.driver.find_elements(*self.STATUS_BOX)
+
+    def wait_until_new_status_appeared(self):
+        # TODO You need to do smart explicit wait!!!
+        time.sleep(5)
--- test_newsfeed_create.py
+++ test_newsfeed_create.py
@@ -6,12 +6,21 @@ from value_models.status import Status
 def test_add_text_status(driver, signed_in_user):
     status = Status(text="Shit happens!!!:(", user=signed_in_user)
     app = OxwallSite(driver)
-    app.add_new_text_status(status.text)
-    app.wait_until_new_status_appeared()
+    old_status_list = app.dash_page.status_list
+
+    app.dash_page.status_text_field.send_keys(status.text)
+    app.dash_page.send_button.click()
+    app.dash_page.wait_until_new_status_appeared()
+
+    new_status_list = app.dash_page.status_list
+    assert len(new_status_list) == len(old_status_list) + 1
+    assert status.text in new_status_list[0].text
+    assert signed_in_user.real_name in new_status_list[0].text
+    assert "within 1 minute" in new_status_list[0].text
     # Verification that new status with this text appeared
-    text_elements = app.get_newsfeed_list()
-    newsfeed_users = app.get_newsfeed_users()
-    newsfeed_times = app.get_newsfeed_times()
-    assert text_elements[0].text == status.text
-    assert newsfeed_users[0].text == status.user.real_name
-    assert newsfeed_times[0].text == "within 1 minute"
+    # text_elements = app.get_newsfeed_list()
+    # newsfeed_users = app.get_newsfeed_users()
+    # newsfeed_times = app.get_newsfeed_times()
+    # assert text_elements[0].text == status.text
+    # assert newsfeed_users[0].text == status.user.real_name
+    # assert newsfeed_times[0].text == "within 1 minute"
